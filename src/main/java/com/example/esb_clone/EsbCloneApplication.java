1272aaecd7b17bca3e3f87b3deaa9dab03d78d17
ohnahcc <qkrcskgh1015@naver.com>
2025-02-13 17:43:42 +0900

    Initial commit

---
+++ src/main/java/com/example/esb_clone/EsbCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.esb_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsbCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsbCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/esb_clone/EsbCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esb_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsbCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
